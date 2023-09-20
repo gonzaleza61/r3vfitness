e50e0d4367ed9ee9c80674660cf330ffce7ccced
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-09-19 21:04:38 -0500

    navbar formatting

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -5,7 +5,15 @@
 export default function NavBar(){
     return(
         <>
-        <div>Hello</div>
+        <nav class='max-w-screen-xl my-0 mx-auto py-16 px-4'>
+            <ul class="m-0 p-0 list-none flex flex-row flex-wrap justify-center items-center gap-8">
+                <li class="basis-auto text-left mr-auto"><h1><a href="/">Home</a></h1></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Programs</a></li>
+                <li><a href="#">Testimonials</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
         </>
     )
 }
