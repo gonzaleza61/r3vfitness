94624c5e19fa6a33cff2a4ff2634bf499c6395f6
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-09-19 22:26:23 -0500

    added inter font

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,17 +1,21 @@
 
+import { Inter } from "next/font/google"
 
-
+const inter = Inter({
+    subsets:['latin'],
+    weight: '200'
+})
 
 export default function NavBar(){
     return(
         <>
-        <nav class='max-w-screen-xl my-0 mx-auto py-16 px-4'>
+        <nav class='max-w-screen-xl my-0 mx-auto py-16 px-4' className={inter.className}>
             <ul class="m-0 p-0 list-none flex flex-row flex-wrap justify-center items-center gap-8">
-                <li class="basis-auto text-left mr-auto"><h1><a href="/">Home</a></h1></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Programs</a></li>
-                <li><a href="#">Testimonials</a></li>
-                <li><a href="#">Contact</a></li>
+                <li class="basis-auto text-left mr-auto"><h1><a href="/">HOME</a></h1></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">PROGRAMS</a></li>
+                <li><a href="#">TESTIMONIALS</a></li>
+                <li><a href="#">CONTACT</a></li>
             </ul>
         </nav>
         </>
