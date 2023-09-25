16388885ba5f26c2b35e0a9f8de6dccdcb53d060
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-09-25 00:38:37 -0500

    added hero image

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -1,11 +1,14 @@
 import { anton } from "../fonts"
+import Image from "next/image"
 
 export default function HeroSection() {
   return (
     <>
-      <h3 className={anton.className}>Welcome to</h3>
-      <h1 className={anton.className}>r3vFitness</h1>
-      <p className="w-full bg-white">fitness</p>
+      <div className="mx-auto flex max-w-screen-xl flex-row items-center justify-center py-16 md:px-4">
+        <h3 className={`${anton.className} my-auto block`}>Welcome to</h3>
+        <h1 className={anton.className}>r3vFitness</h1>
+        <Image src="/r3vfit.jpg" alt="r3vfitlogo" width={600} height={600} />
+      </div>
     </>
   )
 }
