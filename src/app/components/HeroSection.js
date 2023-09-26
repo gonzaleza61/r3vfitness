fff1ad655bece23f7d2d63fde12e6add336c9365
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-09-26 10:50:02 -0500

    hero section responsivness column

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -4,10 +4,10 @@ import Image from "next/image"
 export default function HeroSection() {
   return (
     <>
-      <div className="mx-auto flex max-w-screen-xl flex-row items-center justify-center py-16 md:px-4">
+      <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center py-16 md:flex-row md:px-4">
         <h3 className={`${anton.className} my-auto block`}>Welcome to</h3>
         <h1 className={anton.className}>r3vFitness</h1>
-        <Image src="/r3vfit.jpg" alt="r3vfitlogo" width={600} height={600} />
+        <Image src="/r3vfit.jpg" alt="r3vfitlogo" width={450} height={450} />
       </div>
     </>
   )
