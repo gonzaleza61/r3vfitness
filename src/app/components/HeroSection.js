f0e9b0c9f0399ea78cb63482ac5ed863ef4d2a62
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-10-02 16:18:46 -0500

    lowered letter spacing

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -25,7 +25,7 @@ export default function Component() {
           <div className="flex flex-col justify-center space-y-4 text-center">
             <div className="space-y-2">
               <h1
-                className={`${anton.className} bg-gradient-to-r from-white via-lime-200 to-lime-500 bg-clip-text text-3xl font-bold tracking-tighter text-transparent sm:text-5xl xl:text-6xl/none`}
+                className={`${anton.className} bg-gradient-to-r from-white via-lime-200 to-lime-500 bg-clip-text text-3xl font-bold tracking-tight text-transparent sm:text-5xl xl:text-6xl/none`}
               >
                 Start Your Revolution In Fitness
               </h1>
