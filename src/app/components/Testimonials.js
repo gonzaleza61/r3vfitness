a3744a3455a38607c56d66ef1a55e4bfd3e1d792
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-10-16 15:12:44 -0500

    edited testimonials

--- src/app/components/Testimonials.js
+++ src/app/components/Testimonials.js
@@ -18,7 +18,7 @@ export default function Testimonials() {
               <div className="flex h-full flex-col justify-center space-y-4">
                 <Image
                   className="mx-auto h-20 w-20 rounded-full"
-                  src="/vercel.svg"
+                  src="/aaron.webp"
                   alt="user avatar"
                   height="220"
                   width="220"
@@ -26,20 +26,16 @@ export default function Testimonials() {
                 />
                 <p className="text-black md:text-xl">
                   {" "}
-                  <span className="font-serif"></span> Lorem ipsum dolor sit
-                  amet consectetur adipisicing elit. Quaerat repellat
-                  perspiciatis excepturi est. Non ipsum iusto aliquam
-                  consequatur repellat provident, omnis ut, sapiente voluptates
-                  veritatis cum deleniti repudiandae ad doloribus.{" "}
+                  <span className="font-serif"></span> {'"'}Isaias is the real
+                  deal. His dedication and expertise have been life-changing.
+                  Thanks to his guidance, I{"'"}ve achieved fitness goals I
+                  never thought were possible.{'"'}{" "}
                   <span className="font-serif"></span>
                 </p>
                 <div>
-                  <h6 className="text-lg font-semibold leading-none">
-                    Jane Doe
+                  <h6 className="text-lg font-semibold leading-none text-black">
+                    Aaron G.
                   </h6>
-                  <span className="text-xs text-gray-500">
-                    Product Designer
-                  </span>
                 </div>
               </div>
             </div>
@@ -47,7 +43,7 @@ export default function Testimonials() {
             <div className="rounded-xl border border-gray-100 bg-gray-50 p-6 sm:flex sm:space-x-8 sm:p-8">
               <Image
                 className="mx-auto h-20 w-20 rounded-full"
-                src="/vercel.svg"
+                src="/sarah.webp"
                 alt="user avatar"
                 height="220"
                 width="220"
@@ -56,27 +52,23 @@ export default function Testimonials() {
               <div className="mt-4 space-y-4 text-center sm:mt-0 sm:text-left">
                 <p className="text-black ">
                   {" "}
-                  <span className="font-serif"></span> Lorem ipsum dolor sit
-                  amet consectetur adipisicing elit. Quaerat repellat
-                  perspiciatis excepturi est. Non ipsum iusto aliquam
-                  consequatur repellat provident, omnis ut, sapiente voluptates
-                  veritatis cum deleniti repudiandae ad doloribus.{" "}
+                  <span className="font-serif"></span> Training with Isaias has
+                  been incredible. His customized workouts and constant
+                  motivation are the reasons I{"'"}ve made significant progress
+                  in my fitness journey. I can{"'"}t recommend him enough!
                   <span className="font-serif"></span>
                 </p>
                 <div>
-                  <h6 className="text-lg font-semibold leading-none">
-                    Jane Doe
+                  <h6 className="text-lg font-semibold leading-none text-black">
+                    Sarah M.
                   </h6>
-                  <span className="text-xs text-gray-500">
-                    Product Designer
-                  </span>
                 </div>
               </div>
             </div>
             <div className="rounded-xl border border-gray-100 bg-gray-50 p-6 sm:flex sm:space-x-8 sm:p-8">
               <Image
                 className="mx-auto h-20 w-20 rounded-full"
-                src="/vercel.svg"
+                src="/michael.webp"
                 alt="user avatar"
                 height="220"
                 width="220"
@@ -85,20 +77,17 @@ export default function Testimonials() {
               <div className="mt-4 space-y-4 text-center sm:mt-0 sm:text-left">
                 <p className="text-black ">
                   {" "}
-                  <span className="font-serif"></span> Lorem ipsum dolor sit
-                  amet consectetur adipisicing elit. Quaerat repellat
-                  perspiciatis excepturi est. Non ipsum iusto aliquam
-                  consequatur repellat provident, omnis ut, sapiente voluptates
-                  veritatis cum deleniti repudiandae ad doloribus.{" "}
+                  <span className="font-serif"></span> Isaias is more than a
+                  personal trainer; he{"'"}s a life coach. His passion for
+                  fitness and unwavering support have turned my life around. I
+                  {"'"}m living healthier and happier, all thanks to his
+                  guidance.
                   <span className="font-serif"></span>
                 </p>
                 <div>
-                  <h6 className="text-lg font-semibold leading-none">
-                    Jane Doe
+                  <h6 className="text-lg font-semibold leading-none text-black">
+                    Michael H.
                   </h6>
-                  <span className="text-xs text-gray-500">
-                    Product Designer
-                  </span>
                 </div>
               </div>
             </div>
