08dadef6f093bf39772ac0ab6d1acbfb80eacbdf
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-10-21 12:41:44 -0500

    vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar"
 import Footer from "./components/Footer"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css"
 import { inter } from "./fonts"
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
       <body>
         <NavBar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
