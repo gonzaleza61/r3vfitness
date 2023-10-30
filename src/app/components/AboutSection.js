428e78b8222947f8956dbd25ef0881375ac927ac
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-10-29 21:44:41 -0500

    adding animations to about section;

--- src/app/components/AboutSection.js
+++ src/app/components/AboutSection.js
@@ -1,11 +1,30 @@
+"use client"
 import Image from "next/image"
+import { motion, useInView, useAnimation } from "framer-motion"
+import { useRef, useEffect } from "react"
 
 export default function AboutSection() {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+
+  const mainControls = useAnimation()
+  const slideControls = useAnimation()
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible")
+      slideControls.start("visible")
+    }
+  }, [isInView])
+
   return (
     <>
       <div id="about" className=" bg-white py-16">
         <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-          <div className="space-y-6 md:flex md:gap-6 md:space-y-0 lg:items-center lg:gap-12">
+          <div
+            ref={ref}
+            className="space-y-6 md:flex md:gap-6 md:space-y-0 lg:items-center lg:gap-12"
+          >
             <div className="md:5/12 lg:w-5/12">
               <Image
                 src="/isaias.jpeg"
@@ -17,9 +36,20 @@ export default function AboutSection() {
               />
             </div>
             <div className="md:7/12 lg:w-6/12">
-              <h2 className="text-2xl font-bold text-gray-900 md:text-4xl">
-                About Isaias
-              </h2>
+              <motion.div
+                variants={{
+                  hidden: { opacity: 0, y: 75 },
+                  visible: { opacity: 1, y: 0 },
+                }}
+                initial="hidden"
+                animate={mainControls}
+                transition={{ duration: 0.5, delay: 0.25 }}
+              >
+                <h2 className="text-2xl font-bold text-gray-900 md:text-4xl">
+                  About Isaias
+                </h2>
+              </motion.div>
+
               <p className="mt-6 text-gray-600">
                 Isaias{"'"} role is to motivate, facilitate and make training a
                 fun and rewarding experience for his clients. He has a passion
