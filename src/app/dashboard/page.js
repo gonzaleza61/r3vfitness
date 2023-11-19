89f00333415bab94c3f9ff05bdc8524a6f3467bc
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-11-19 15:23:59 -0600

    added supabase keys

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,11 +1,36 @@
 "use client"
-import { useUser } from "@clerk/nextjs"
+import { useUser, useAuth } from "@clerk/nextjs"
+import { createClient } from "@supabase/supabase-js"
 import { useEffect, useState } from "react"
+
+const supabaseClient = async (supabaseAccessToken) => {
+  const supabase = createClient(
+    process.env.NEXT_SUPABASE_CLIENT_URL,
+    process.env.NEXT_SUPABASE_KEY,
+    {
+      global: { headers: { Authorization: `Bearer ${supabaseAccessToken}` } },
+    }
+  )
+  return supabase
+}
 
 export default function Dashboard() {
   const { user } = useUser()
+  const { getToken } = useAuth()
   const [userName, setUserName] = useState("")
 
+  const fetchData = async () => {
+    // TODO #1: Replace with your JWT template name
+    const supabaseAccessToken = await getToken({ template: "supabase" })
+
+    const supabase = await supabaseClient(supabaseAccessToken)
+
+    // TODO #2: Replace with your database table name
+
+    const { data, error } = await supabase.from("your_table").select()
+
+    // TODO #3: Handle the response
+  }
   useEffect(() => {
     if (user) {
       const userID = user.id
